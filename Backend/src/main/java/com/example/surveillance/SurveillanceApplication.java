0d8620b3a3db6b3c665aeb01c563e8147bc3dd86
walid-mataich <walidmataich1@gmail.com>
2025-03-14 20:35:20 +0000

    fix

--- Backend/src/main/java/com/example/surveillance/SurveillanceApplication.java
+++ Backend/src/main/java/com/example/surveillance/SurveillanceApplication.java
@@ -1,6 +1,6 @@
 package com.example.surveillance;
 
-import io.github.cdimascio.dotenv.Dotenv;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SurveillanceApplication {
 
 	public static void main(String[] args) {
-
-		// Charger les variables d'environnement depuis .env
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("OPENAI_API_KEY", dotenv.get("OPENAI_API_KEY"));
 
 		SpringApplication.run(SurveillanceApplication.class, args);
 		
